fba308ef9f2921d50f5d3a4a240ab7cb2ae7b5f2
Nishant Raj <nishantraj2109@gmail.com>
2024-11-19 19:33:32 +0530

    updated api link

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/login", {
+      const response = await fetch("https://practice-facebook-ui.onrender.com/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
